2cf1095f475e0af027325bb5ac2eb68be7d09bc4
lietxia <lietxia@gmail.com>
2020-11-01 19:48:59 +0800

    test

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 const http = require('http');
 const fs = require('fs');
 var process = require('child_process');
+const server_url = "http://127.0.0.1:8081/";
 
 async function reply_text(text, data) {
     return get_data(
-        "http://127.0.0.1/" +
+        server_url +
         (data.message_type === "group"
             ? "send_group_msg?group_id=" + data.group_id
             : "send_private_msg?user_id=" + data.user_id)
@@ -64,9 +65,9 @@ const server = http.createServer(
 server.listen(8082);
 async function reply_from(data) {
     if (data.post_type != "message") { return; }
-   
+
     if (data.raw_message.startsWith("gb")) {
-        var cmd = `/ home / lede / gb "${data.raw_message.substr(2).trim()}"`;
+        var cmd = `/home/lede/gb "${data.raw_message.substr(2).trim()}"`;
         return process.exec(cmd, function (error, stdout, stderr) {
             console.log("error:" + error);
             console.log("stdout:" + stdout);
